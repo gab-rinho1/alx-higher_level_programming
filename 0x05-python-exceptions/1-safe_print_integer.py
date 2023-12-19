8ea89d05932206a103b3713afd8a1f18d2e782a7
gab-rinho1 <gri75407@gmail.com>
2023-12-19 03:49:25 -0800

    col

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -12,7 +12,7 @@ def safe_print_integer(value):
     """
     try:
         print("{:d}".format(value))
-        return(True)
-    except(TypeError, ValueError):
-        return(False)
+        return True
+    except (TypeError, ValueError):
+        return False
 
